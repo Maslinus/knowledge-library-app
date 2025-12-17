14b2d2152bb4a5b95bf703115bf3dc5e2bb078c9
Maslinus <sulejmanov.daniyar@mail.ru>
2025-12-17 16:09:27 +0300

    .

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,10 +1,8 @@
-// backend/api/index.js
 import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import serverless from "serverless-http";
+import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -15,20 +13,20 @@ import uploadRouter from "../routes/upload.route.js";
 import groupRouter from "../routes/noteGroup.route.js";
 import versionRouter from "../routes/noteVersion.route.js";
 
+import connectToDatabase from "../utils/connectDB.js";
+
 dotenv.config();
 
 const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 app.use('/favicon.ico', express.static(path.join(__dirname, 'favicon.ico')));
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: ["http://localhost:5173"],
-  credentials: true
-}));
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 
 app.use("/api/auth", authRouter);
 app.use("/api/note", noteRouter);
@@ -37,30 +35,15 @@ app.use("/api/uploadCloud", uploadRouter);
 app.use("/api/group", groupRouter);
 app.use("/api/versions", versionRouter);
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Backend is running!" });
-});
+app.get("/api", (req, res) => res.json({ message: "Backend is running!" }));
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
-  return res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message,
-  });
+  return res.status(statusCode).json({ success: false, statusCode, message });
 });
 
-let conn = null;
-
-const connectDB = async () => {
-  if (conn) return conn;
-  conn = await mongoose.connect(process.env.MONGO_URL);
-  console.log("Connected to MongoDB");
-  return conn;
-};
-
 export default async function handler(req, res) {
-  await connectDB();
+  await connectToDatabase(process.env.MONGO_URL);
   return serverless(app)(req, res);
 }
